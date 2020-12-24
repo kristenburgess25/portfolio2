af7cef070e91ef7bfd2a444e33ee48765f362902
Kristen Burgess <kristenburgess25@Kristens-iMac.local>
2020-12-23 18:59:23 -0700

    add helper data files

---
+++ src/helpers/clients.js
@@ -0,0 +1,33 @@
+export default [
+    {
+      body: 'This simple weather app uses Google geolocation API to find current location and access local weather. The user can also "Pin" or save certain cities.',
+      styleclass: 'perpro',
+      technologies: 'React.js, Redux, Jest',
+      title: 'OMNIQUE',
+      url: 'https://github.com/kswhyte/weather-tracker',
+      liveUrl: 'https://kswhyte.github.io/weather-tracker',
+    },
+    {
+      body: 'A fall-themed take on the classic game.',
+      styleclass: 'crunchy-ratings',
+      technologies: 'JavaScript/jQuery',
+      title: 'LOS',
+      url: 'https://github.com/kristenburgess25/game-time'
+    },
+    {
+      body: 'A simple task manager, allowing users to add tasks to a ToDo list, rank them by importance, and sort/filter/search accordingly.',
+      styleclass: 'napkin-app',
+      technologies: 'jQuery, SASS/SCSS, Selenium',
+      title: '📝 2DoBox',
+      url: 'https://github.com/kristenburgess25/2DoBox-Pivot'
+    },
+    {
+      body: 'A basic contact management app, intended for use at networking events. Users sign in through Google, and their contacts are stored in a user-specific Google Firebase database.',
+      styleclass: 'amptron',
+      technologies: 'React.js, Google Firebase, Enzyme, SASS/SCSS',
+      title: '🤝 NimbleNetwork',
+      url: 'https://github.com/kswhyte/nimble-network',
+      liveUrl: 'https://nimblenetwork-d13c3.firebaseapp.com/'
+    },
+  ]
+  
---
+++ src/helpers/projectsHelpers.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    body: 'This simple weather app uses Google geolocation API to find current location and access local weather. The user can also "Pin" or save certain cities.',
+    styleclass: 'perpro',
+    technologies: 'React.js, Redux, Jest',
+    title: '⛅️ Weather App',
+    url: 'https://github.com/kswhyte/weather-tracker',
+    liveUrl: 'https://kswhyte.github.io/weather-tracker',
+  },
+  {
+    body: 'A fall-themed take on the classic game.',
+    styleclass: 'crunchy-ratings',
+    technologies: 'JavaScript/jQuery',
+    title: '🎃 Hangman Game',
+    url: 'https://github.com/kristenburgess25/game-time'
+  },
+  {
+    body: 'A simple task manager, allowing users to add tasks to a ToDo list, rank them by importance, and sort/filter/search accordingly.',
+    styleclass: 'napkin-app',
+    technologies: 'jQuery, SASS/SCSS, Selenium',
+    title: '📝 2DoBox',
+    url: 'https://github.com/kristenburgess25/2DoBox-Pivot'
+  },
+  {
+    body: 'A basic contact management app, intended for use at networking events. Users sign in through Google, and their contacts are stored in a user-specific Google Firebase database.',
+    styleclass: 'amptron',
+    technologies: 'React.js, Google Firebase, Enzyme, SASS/SCSS',
+    title: '🤝 NimbleNetwork',
+    url: 'https://github.com/kswhyte/nimble-network',
+    liveUrl: 'https://nimblenetwork-d13c3.firebaseapp.com/'
+  },
+  // {
+  //   body: 'A logic game with the objective of turning off all of the lights.',
+  //   styleclass: 'lights-out',
+  //   technologies: 'jQuery, Mocha, Chai, Object Oriented Programming',
+  //   title: '🚙  Omnique',
+  //   url: 'https://github.com/bgdfry/game-time-lights-out'
+  // },
+  // {
+  //   body: 'An npm package that allows users to convert their local audio files to dataURLs for use via Electron.',
+  //   styleclass: 'eac',
+  //   technologies: 'Javascript, Babel',
+  //   title: '🎶 electron-audio-conversion',
+  //   url: 'https://github.com/Jeff-Duke/electron-audio-conversion'
+  // }
+]
