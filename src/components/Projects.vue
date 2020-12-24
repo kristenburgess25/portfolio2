b44c3ab2f440e3ca00e994f6812ab9c0797854c5
Kristen Burgess <kristenburgess25@Kristens-iMac.local>
2020-12-23 19:03:14 -0700

    Add Projects component

---
+++ src/components/Projects.vue
@@ -0,0 +1,31 @@
+<template>
+  <app-section :styleclass="styleclass">
+    <div class="section-title-wrapper">
+      <h1 class="section-title">Projects</h1>
+      <div class="title-underline"></div>
+    </div>
+    <card-list></card-list>
+  </app-section>
+</template>
+
+<script>
+import Section from './Section.vue';
+import CardList from './CardList.vue';
+export default {
+  name: 'Projects',
+  components: {
+    'app-section': Section,
+    'card-list': CardList,
+  },
+  data () {
+    return {
+      styleclass: 'projects-tile',
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="css">
+</style>
